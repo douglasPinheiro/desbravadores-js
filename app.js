101a5dc7500c679a8048752b64f16ac49441d56c
Henrique Belfort <henrique.sys@gmail.com>
2015-09-14 09:38:33 -0300

    usabilidade inicial para CRUD

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var db = require('./model/db');
-var member = require('./model/members');
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
+
+var db = require('./model/db'),
+    member = require('./model/members');
+
+var routes = require('./routes/index'),
+    members = require('./routes/members');
 
 var app = express();
 
@@ -24,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/members', members);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
