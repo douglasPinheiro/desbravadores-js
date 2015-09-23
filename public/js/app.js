ba417fc8300fa592af23b4232455642f65a82b18
Henrique Belfort <henrique.sys@gmail.com>
2015-09-23 17:45:12 -0300

    making some progress on emberjs

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,19 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+    this.route('list-members', { path: 'listagem-de-membros' })
+});
 
+App.ListMembersRoute = Ember.Route.extend({
+    
+});
+
+App.ListMembersController = Ember.Controller.extend({
+    pageName: 'Listagem de membros'
 });
 
 App.IndexRoute = Ember.Route.extend({
-    model: function() {
-        return 'Bem vindo ao Desbravadores.js';
+    model: function () {
+        return 'Bem vindo ao Desbravadores.JS';
     }
 });
