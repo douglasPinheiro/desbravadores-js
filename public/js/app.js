dba99ab65abcb0854b3539fe8d82b0f15c664400
Henrique Belfort <henrique.sys@gmail.com>
2015-09-24 17:33:12 -0300

    member list and integration between express e ember

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,24 @@ App.Router.map(function() {
 });
 
 App.ListMembersRoute = Ember.Route.extend({
-    
+    model: function() {
+        var url = 'http://localhost:3000/members';
+        return Ember.$.getJSON(url).then(function(data) {
+            return data;
+        });
+    }
 });
 
 App.ListMembersController = Ember.Controller.extend({
-    pageName: 'Listagem de membros'
+    pageName: 'Listagem de membros',
+
 });
 
 App.IndexRoute = Ember.Route.extend({
-    model: function () {
-        return 'Bem vindo ao Desbravadores.JS';
+    model: function() {
+        var url = 'https://api.github.com/repos/henriquebelfort/desbravadores-js/commits';
+        return Ember.$.getJSON(url).then(function(data) {
+            return data.splice(0, 5);
+        });
     }
 });
