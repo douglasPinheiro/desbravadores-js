1040756fab84bcbe9bbb9f78a51ee837589ed003
Henrique Belfort <henrique.sys@gmail.com>
2015-10-08 12:01:08 -0300

    Change member for DS typed model

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,13 @@
 //import DS from 'ember-data';
-
 App = Ember.Application.create({ LOG_TRANSITIONS: true });
 
 App.Router.map(function() {
-    this.route('list-members', { path: 'listagem-de-membros' });
-    this.route('create-member', { path: 'criacao-de-membro' });
+  this.route('list-members', { path: 'listagem-de-membros' });
+  this.route('create-member', { path: 'criacao-de-membro' });
 });
 
 App.Store = DS.Store.extend({
-  adapter: 'App.ApplicationAdapter'
+  adapter: DS.RESTAdapter,
 });
 
 DS.RESTAdapter.reopen({
@@ -16,48 +15,44 @@ DS.RESTAdapter.reopen({
 });
 
 App.ListMembersRoute = Ember.Route.extend({
-    model: function() {
-        return Ember.$.getJSON('/members').then(function(data) {
-            return data;
-        });
-    }
+  model: function() {
+    return Ember.$.getJSON('/members').then(function(data) {
+      return data;
+    });
+  }
 });
 
-App.Member = Ember.Object.extend({
-    name : "",
-    email : "",
-    birthday : "",
-    phone : "",
-    cellphone : "",
+App.Member = DS.Model.extend({
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  birthday: DS.attr('date'),
+  phone: DS.attr('string'),
+  cellphone: DS.attr('string'),
 });
 
 App.CreateMemberRoute = Ember.Route.extend({
-    model: function(){
-        return App.Member.create();
-    },
-    setupController : function(controller, model){
-        controller.set("model", model);
-    }
-});
-
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-    host: 'https://localhost:3000'
+  model: function() {
+    return this.store.createRecord('member');
+  },
+  setupController: function(controller, model) {
+    controller.set("model", model);
+  }
 });
 
 App.CreateMemberController = Ember.Controller.extend({
-    actions: {
-        submit: function() {
-            var obj = this.get('model');
-            console.info(JSON.stringify(obj));
-        }
+  actions: {
+    submit: function() {
+      var obj = this.get('model');
+      console.info(JSON.stringify(obj));
     }
+  }
 });
 
 App.IndexRoute = Ember.Route.extend({
-    model: function() {
-        var url = 'https://api.github.com/repos/henriquebelfort/desbravadores-js/commits';
-        return Ember.$.getJSON(url).then(function(data) {
-            return data.splice(0, 5);
-        });
-    }
+  model: function() {
+    var url = 'https://api.github.com/repos/henriquebelfort/desbravadores-js/commits';
+    return Ember.$.getJSON(url).then(function(data) {
+      return data.splice(0, 5);
+    });
+  }
 });
